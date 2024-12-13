e5273ffc4bc6a8e3f918834cfe476f6d00b5017c
sayan chakraborty <sayanchakraborty213@gmail.com>
2024-12-13 06:40:44 +0000

    container config done

---
+++ 01/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=12345;
+let accountEmail="sayan@gmail.com";
+var accountPass="2136";
+accountCity="kolkata";
+let accountState;
+
+
+// accountId=666;
+// console.log(accountId);
+
+accountEmail="sayan125@gmail.com";
+accountPass="2365";
+accountCity="london";
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState]);
+
