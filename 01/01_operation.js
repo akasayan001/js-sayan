fae33d08abbfeb4f92f63ae469c1b65e0c1578de
sayan chakraborty <sayanchakraborty213@gmail.com>
2024-12-14 18:13:36 +0000

    operation

---
+++ 01/01_operation.js
@@ -0,0 +1,17 @@
+ let value=3
+ let negativeValue=-value
+ console.log(negativeValue);
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**3);
+ console.log(2/3);
+ console.log(2%3);
+ console.log("1"+2);
+ console.log("1"+2+2);
+ console.log(2+2+"1");
+ let gameCounter=100
+ gameCounter++
+ console.log(gameCounter);
+ ++gameCounter
+ console.log(gameCounter);
