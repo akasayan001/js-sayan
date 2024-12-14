f94084f6d84ce22f5a0e21573188126753751e4c
sayan chakraborty <sayanchakraborty213@gmail.com>
2024-12-14 17:49:09 +0000

    convertion

---
+++ 01/01_convertion.js
@@ -0,0 +1,22 @@
+let score="33mk"
+let she=true
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+console.log(typeof she);
+let he=Number(she)
+console.log(typeof(he));
+console.log(he);
+let loggIn=50
+let booleanloggIn= Boolean(loggIn)
+console.log(booleanloggIn);
+let someNumber=55
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+console.table([score,stringNumber,booleanloggIn,he,valueInNumber])
+
+
