d59f401fb8ca2b4e7f6ec6edfa08610ea466c01e
sayan chakraborty <sayanchakraborty213@gmail.com>
2024-12-17 12:37:46 +0000

    compare again datatype

---
+++ datatype.summary.js
@@ -0,0 +1,28 @@
+// Primitive
+
+// 7 type
+// string Number Boolean Null Undefind Symbol BigInt
+
+// Reference(non Primitive)
+
+//Array Object Function
+
+
+let num=1368643467643649673643646936n
+console.log(typeof num);
+
+const id=Symbol("123")
+const anotherId=Symbol(123)
+console.log(id===anotherId);
+
+const hero=["me","you","zyan","thapa"];
+let myObj={
+    name:"sayan",
+    age:22
+}
+const myFunction=function(){
+    console.log("hello sayan");
+    
+}
+console.log(myObj);
+
