6beb32cc008223d73378f8929e69a3968dc204ae
sayan chakraborty <sayanchakraborty213@gmail.com>
2024-12-17 14:40:37 +0000

    string

---
+++ 01/06_String.js
@@ -0,0 +1,37 @@
+let name="sayan "
+let age=22
+// console.log(name+age+" hello");
+console.log(`my name is ${name} and my age is ${age}`);
+
+let game= new String("sayan");
+console.log(game[0]);
+console.log(game.__proto__);
+console.log(game.length);
+console.log(game.toLocaleLowerCase());
+console.log(game.toUpperCase());
+console.log(game.charAt(3));
+console.log(game.indexOf('a'));
+console.log(game.substring(0,4));
+console.log(game.slice(-9,2));
+let game1=new String("   sayaln   ")
+console.log(game1.trim());
+ let l=game1.replace("l","")
+console.log(l);
+console.log(game.includes("sayan"));
+
+// String to Array(basic of _)
+let me="i_am_sayan";
+console.log(me.split("_"));
+
+
+
+
+
+
+
+
+
+
+
+
+
