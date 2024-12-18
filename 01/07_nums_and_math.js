f4e23b6c944e71fb9d61b5fa1659d327ef79854d
sayan chakraborty <sayanchakraborty213@gmail.com>
2024-12-18 15:19:21 +0000

    nums and math

---
+++ 01/07_nums_and_math.js
@@ -0,0 +1,39 @@
+//-----------------Nums-------------------//
+let score=400
+console.log(score);
+let balance=new Number(500)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(4));
+let num=415.896
+console.log(num.toPrecision(4));
+console.log(num.toPrecision(3));
+let number=1000000
+console.log(number.toLocaleString());
+console.log(number.toLocaleString('en-IN'));
+console.log();
+console.log();
+
+
+
+//---------------------math-----------------//
+
+console.log(Math);
+console.log(Math.abs(-9));
+console.log(Math.round(4.3));
+console.log(Math.round(4.7));
+console.log(Math.min(7,8,9,5,3,54,47));
+console.log(Math.max(7,8,9,5,3,54,47));
+console.log();
+console.log();
+
+
+
+
+
+
+
+
+
+
