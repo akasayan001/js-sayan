072d128dc0e92cdd42078eb79465014d89ff26fc
sayan chakraborty <sayanchakraborty213@gmail.com>
2024-12-18 16:32:55 +0000

    array

---
+++ 02/01_Array.js
@@ -0,0 +1,26 @@
+let arr=[6,5,4,8,9,7]
+console.log(arr[5]);
+let myarr=new Array(2,6,4,8)
+console.log(myarr);
+
+ //                array method
+
+let newarr=[1,2,3,4,5,6]
+newarr.push(9);
+console.log(newarr);
+newarr.push(7);
+console.log(newarr);
+newarr.pop();
+console.log(newarr);
+newarr.unshift(9);
+console.log(newarr);
+newarr.shift();
+console.log(newarr);
+
+console.log(newarr.includes(23));
+console.log(newarr.indexOf(3));
+let newArray=newarr.join() //join two array and return type is String
+console.log(newArray);
+
+
+
